b9deef5e11ad62c967fa2e226314602ae26732b2
Thor Whalen <thorwhalen1@gmail.com>
2021-10-14 10:32:20 -0700

    0.0.3:

--- know/__init__.py
+++ know/__init__.py
@@ -3,6 +3,11 @@ Build live stream tools
 
 Essentially being able to do this:
 
+The tools are made to be able to create live streams of data
+(e.g. from sensors) and funnel them into proceses with a consistent interfaces.
+One important process being the process that will store all or part of
+the data, through a simple storage-system-agnositic facade.
+
 ```python
 proc = LiveProc(
    source=Sources(  # make a multi-source object (which will manage buffering and timing)
