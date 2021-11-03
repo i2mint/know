74a358b52b666495360649d11b633ac4b5e35a78
Thor Whalen <thorwhalen1@gmail.com>
2021-11-03 15:51:39 -0700

    scrap: more and more

--- know/scrap/architectures.py
+++ know/scrap/architectures.py
@@ -3,12 +3,25 @@
 from dataclasses import dataclass
 from typing import Callable, Iterable, Any, Mapping
 from atypes import Slab, MyType
-from i2.multi_object import FuncFanout
+from i2.multi_object import FuncFanout, ContextFanout, MultiObj
 
 Service = MyType(
-    'Consumer', Callable[[Slab], Any], doc="A function that will call slabs iteratively"
+    'Consumer', Callable[[Slab], Any], doc='A function that will call slabs iteratively'
 )
 Name = str.isidentifier()
+BoolFunc = Callable[[...], bool]
+FiltFunc = Callable[[Any], bool]
+
+def let_through(x):
+    return x
+
+class MultiIterator(MultiObj):
+    def _gen_next(self):
+        for name, iterator in self.objects.items():
+            yield name, next(iterator, None)
+
+    def __next__(self) -> dict:
+        return dict(self._gen_next())
 
 # TODO: Default consumer(s) (e.g. data-safe prints?)
 # TODO: Default slabs? (iterate through
@@ -23,15 +36,57 @@ class WithSlabs:
         else:
             # TODO: Add capability (in FuncFanout) to get a mix of (un)named consumers
             self.multi_service = FuncFanout(**self.services)
+        self.slabs_and_services_context = ContextFanout(
+            slabs=self.slabs, **self.multi_service
+        )
 
     def __iter__(self):
-        for slab in self.slabs:
-            yield self.multi_service(slab)
+        with self.slabs_and_services_context:
+            for slab in self.slabs:
+                yield self.multi_service(slab)
 
-    def __call__(self, callback=None):
+    def __call__(self, callback: Callable = None, sentinel_func: FiltFunc = None):
         for multi_service_output in self:
             if callback:
-                callback(multi_service_output)
+                callback_output = callback(multi_service_output)
+                if sentinel_func:
+                    if sentinel_func(callback_output):
+                        break
 
 
+def test_multi_iterator():
+    # get_multi_iterable = lambda: MultiIterable(
+    #     audio=iter([1, 2, 3]), keyboard=iter([4, 5, 6])
+    # )
 
+    def is_none(x):
+        return x is None
+
+    def is_not_none(x):
+        return x is not None
+
+    # Note: Equivalent to any_non_none_value = Pipe(methodcaller('values'), iterize(
+    # is_not_none), any)
+    def any_non_none_value(d: dict):
+        """True if and only if d has any non-None values
+
+        >>> assert not any_non_none_value({'a': None, 'b': None})
+        >>> assert any_non_none_value({'a': None, 'b': 3})
+        """
+        return any(map(is_not_none, d.values()))
+
+    # Note: Does not work (never stops)
+    # get_multi_iterable = lambda: MultiIterable(
+    #     audio=iter([1, 2, 3]),
+    #     keyboard=iter([4, 5, 6])
+    # )
+
+    slabs = MultiIterator(audio=iter([1, 2, 3]), keyboard=iter([4, 5, 6]))
+    services = {'let_through': let_through, 'log': print}
+    app = WithSlabs(slabs=slabs, services=services)
+
+    assert list(app) == [
+        {'audio': 1, 'keyboard': 4},
+        {'audio': 2, 'keyboard': 5},
+        {'audio': 3, 'keyboard': 6},
+    ]
