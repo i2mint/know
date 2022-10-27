a67419d5ca3a9dd1e535c38e2a903074a277785d
sylvainbonnot <sppbonnot@gmail.com>
2022-10-27 14:48:45 +0200

    update

--- know/examples/keyboard_and_audio_front.py
+++ know/examples/keyboard_and_audio_front.py
@@ -1,10 +1,32 @@
 from know.examples.keyboard_and_audio import keyboard_and_audio
+from i2.wrapper import arg_val_converter
+from i2 import Sig
+
+# TODO: Generalize to a curriable alias handling function? It's a if_cond_val pattern.
+def none_if_none_string(x):
+    if isinstance(x, str) and x == "None":
+        return None
+    return x
 
 
-if __name__ == '__main__':
+def none_if_none_string_for_all_none_defaulted(func, exception_argnames=()):
+    none_defaulted_args = [
+        name
+        for name, default in Sig(func).defaults.items()
+        if default == None and name not in exception_argnames
+    ]
+    if none_defaulted_args:
+        return arg_val_converter(
+            func, **{name: none_if_none_string for name in none_defaulted_args}
+        )
+    return func
+
+
+# config
+if __name__ == "__main__":
     from streamlitfront import mk_app
 
-    app = mk_app([keyboard_and_audio])
+    _keyboard_and_audio = none_if_none_string_for_all_none_defaulted(keyboard_and_audio)
+
+    app = mk_app([_keyboard_and_audio])
     app()
-
-
